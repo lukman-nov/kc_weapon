4b4e5b3f9747791f09f4e73af81327040cc40bb5
lukman-nov <novawiyadi@gmail.com>
2023-03-08 10:27:43 +0700

    Update Weapon List

--- config.lua
+++ config.lua
@@ -87,23 +87,15 @@ Config.Weapons = {
   },
 
   -- [[ PISTOL ]] --
-  [`WEAPON_REVOLVER`] = {
+  [`WEAPON_REVOLVER`] = { -- POLICE
     model = `WEAPON_REVOLVER`,
     hash = -1045183535,
-    damage = 0.3, 
+    damage = 0.35, 
     disableCritical = true,
     recoil = 0.85
   },
 
-  [`WEAPON_PISTOL`] = {
-    model = `WEAPON_PISTOL`,
-    hash = 453432689,
-    damage = 0.3, 
-    disableCritical = true,
-    recoil = 0.1
-  },
-
-  [`WEAPON_APPISTOL`] = {
+  [`WEAPON_APPISTOL`] = { -- POLICE
     model = `WEAPON_APPISTOL`, 
     hash, 584646201,
     damage = 0.45, 
@@ -111,42 +103,50 @@ Config.Weapons = {
     recoil = 0.1
   },
 
-  [`WEAPON_PISTOL50`] = {
+  [`WEAPON_CERAMICPISTOL`] = { -- POLICE
+    model = `WEAPON_CERAMICPISTOL`, 
+    hash, 727643628,
+    damage = 0.75, 
+    disableCritical = true,
+    recoil = 0.1
+  },
+
+  [`WEAPON_REVOLVER_MK2`] = { -- BADSIDE
+    model = `WEAPON_REVOLVER_MK2`,
+    hash = -879347409,
+    damage = 0.25, 
+    disableCritical = true,
+    recoil = 0.85
+  },
+
+  [`WEAPON_PISTOL50`] = { -- BADSIDE
     model = `WEAPON_PISTOL50`,
     hash = -1716589765,
-    damage = 0.45, 
+    damage = 0.65, 
     disableCritical = true,
     recoil = 0.18
   },
 
-  [`WEAPON_HEAVYPISTOL`] = {
-    model = `WEAPON_HEAVYPISTOL`,
-    hash = -771403250,
-    damage = 0.5, 
-    disableCritical = true,
-    recoil = 0.18
-  },
-
-  [`WEAPON_MACHINEPISTOL`] = {
+  [`WEAPON_MACHINEPISTOL`] = { -- BADSIDE
     model = `WEAPON_MACHINEPISTOL`,
     hash = -619010992,
-    damage = 0.55, 
+    damage = 0.39, 
     disableCritical = true,
     recoil = 0.1
   },
 
   -- [[ SMG ]] --
-  [`WEAPON_MINISMG`] = {
+  [`WEAPON_MINISMG`] = { -- BADSIDE
     model = `WEAPON_MINISMG`,
-    hash = 	324215364,
+    hash = -1121678507,
     damage = 0.75, 
     disableCritical = true,
-    recoil = 0.1
+    recoil = 0.18
   },
-  [`WEAPON_SMG`] = {
+  [`WEAPON_SMG`] = { -- POLICE
     model = `WEAPON_SMG`,
     hash = 736523883,
-    damage = 0.75, 
+    damage = 0.55, 
     disableCritical = true,
     recoil = 0.1
   },
@@ -156,9 +156,17 @@ Config.Weapons = {
   [`WEAPON_CARBINERIFLE`] = {
     model = `WEAPON_CARBINERIFLE`,
     hash = -2084633992,
-    damage = 0.75, 
+    damage = 0.45, 
     disableCritical = true,
-    recoil = 0.15
+    recoil = 0.08
+  },
+
+  [`WEAPON_ASSAULTRIFLE`] = {
+    model = `WEAPON_ASSAULTRIFLE`,
+    hash = -1074790547,
+    damage = 0.50, 
+    disableCritical = true,
+    recoil = 0.13
   },
 
   -- [[ SHOUTGUN ]] --
@@ -167,6 +175,14 @@ Config.Weapons = {
     model = `WEAPON_BULLPUPSHOTGUN`, 
     hash = 	-1654528753,
     damage = 0.8,
+    disableCritical = true,
+    recoil = 0.8
+  },
+
+  [`WEAPON_PUMPSHOTGUN`] = {
+    model = `WEAPON_PUMPSHOTGUN`, 
+    hash = 	487013001,
+    damage = 0.27,
     disableCritical = true,
     recoil = 0.8
   },
@@ -180,7 +196,7 @@ Config.Weapons = {
     recoil = 0.5
   },
 
-  [`WEAPON_SNIPERRIFLE`] = {
+  [`WEAPON_SNIPERRIFLE`] = { -- 
     model = `WEAPON_SNIPERRIFLE`,
     hash = 100416529,
     damage = 2.0, 
