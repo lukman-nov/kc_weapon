d12939b6f7746a86a4000edd65c7c4dd7f23cfda
lukman-nov <novawiyadi@gmail.com>
2023-04-19 05:09:54 +0700

    bumb version v1.0.3

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,7 +3,7 @@ game 'gta5'
 
 name 'kc_weapon'
 author 'Lukman_Nov#5797'
-version '1.0.2'
+version '1.0.3'
 repository 'https://github.com/lukman-nov/kc_weapon'
 description 'Weapon Damage & Recoil Adjusment'
 
