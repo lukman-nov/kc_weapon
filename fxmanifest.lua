50bec3b1f9b8ea26c667a98650f57b56dca77c6e
lukman-nov <novawiyadi@gmail.com>
2023-10-27 11:21:47 +0700

    chore:(fxmanifest.lua) bumb version

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,7 +3,7 @@ game            'gta5'
 
 name            'kc_weapon'
 author          'Lukman_Nov#5797'
-version         '2.3.0'
+version         '2.3.1'
 description     'Weapon Core'
 lua54           'yes'
 
